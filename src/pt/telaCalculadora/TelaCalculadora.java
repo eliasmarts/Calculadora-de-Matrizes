856bcfc648c84399473a55831cecb66e8dca23bf
Elias Santos Martins <e247057@dac.unicamp.br>
2022-07-21 16:48:13 -0300

    imprime matriz

--- src/pt/telaCalculadora/TelaCalculadora.java
+++ src/pt/telaCalculadora/TelaCalculadora.java
@@ -3,16 +3,26 @@ package pt.telaCalculadora;
 import pt.controleCalculo.ICalculoMatriz;
 
 public class TelaCalculadora implements ITelaCalculadora {
+	private ICalculoMatriz controleCalculo;
 
-	@Override
+
 	public void connect(ICalculoMatriz controleCalculo) {
-		// TODO Auto-generated method stub
-
+		this.controleCalculo = controleCalculo;
 	}
+	
+	
+	
 
 	@Override
 	public void iniciar() {
 		System.out.println("Iniciando calculadora");
+		String[][] teste = {
+				{"12", "2", "3"},
+				{"57", "3", "22"},
+				{"12", "0", "7"}
+		};
+		
+		imprimirMatriz(teste);
 	}
 
 	@Override
@@ -21,4 +31,13 @@ public class TelaCalculadora implements ITelaCalculadora {
 
 	}
 
+	
+	private void imprimirMatriz(String[][] matriz) {
+		for (String[] linha : matriz) {
+			for (String valor : linha) {
+				System.out.print(valor + " ");
+			}
+			System.out.println();
+		}
+	}
 }
