cc4cd55d04f7017927cebeb5cfba6745b8cf33e1
Elias Santos Martins <e247057@dac.unicamp.br>
2022-07-22 21:13:59 -0300

    ....

--- src/pt/matriz/IOperacoesMatriz.java
+++ src/pt/matriz/IOperacoesMatriz.java
@@ -1,5 +1,19 @@
 package pt.matriz;
 
-public interface IOperacoesMatriz {
+import pt.operavel.IOperavel;
 
+public interface IOperacoesMatriz {
+	public IMatriz somar(IMatriz matriz);
+	
+	
+	public IMatriz subtrair(IMatriz matriz);
+	
+	
+	public IMatriz multiplicar(IMatriz matriz);
+	
+	
+	public IMatriz multiplicar(IOperavel operavel);
+	
+	
+	public IMatriz clone();
 }
